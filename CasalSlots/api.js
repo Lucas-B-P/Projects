0d4a2925b5a5fbf099a5d18f1fe8afc9a2946aa5
Lucas-B-P <eolucasmlk22@gmail.com>
2024-10-01 15:19:54 -0400

    api.js

--- CasalSlots/api.js
+++ CasalSlots/api.js
@@ -1,18 +1,3 @@
-function isIOS() {
-    return /iPhone|iPad|iPod/i.test(navigator.userAgent);
-}
-
-function checkLowPowerMode() {
-    if (navigator.userAgent.includes("iPhone")) {
-        alert("Para uma melhor experiência, desative o modo de baixo consumo de energia nas configurações do seu iPhone.");
-    }
-}
-
-
-window.onload = function() {
-    checkLowPowerMode();
-};
-
 const updateTimeElement = document.getElementById('update-time');
 let tempoRestante;
 
